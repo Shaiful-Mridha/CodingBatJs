9b42435dbee0b8512a89fa9b05d1917860074abd
Shaiful Mridha <shaifuldu555@gmail.com>
2022-03-31 11:55:15 +0300

    Added countXX & frontTimes

---
+++ WarmUp2/countXX.js
@@ -0,0 +1,23 @@
+/*
+Count the number of 'xx' in the given string.
+We'll say that overlapping is allowed,
+so 'xxx' contains 2 'xx'.
+
+Examples
+
+countXX('abcxx') → 1
+countXX('xxx') → 2
+countXX('xxxx') → 3
+*/
+
+function countXX(str) {
+
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str.substring(i, i + 2) === "xx") {
+      count = count + 1;
+    }
+  }
+  return count;
+}
+console.log(countXX("xxxxx"));
---
+++ WarmUp2/frontTimes.js
@@ -0,0 +1,24 @@
+/*
+Given a string and a non-negative int n, we'll say that
+the front of the string is the first 3 chars, or
+whatever is there if the string is less than length 3.
+Return n copies of the front.
+
+Examples
+
+frontTimes('Chocolate', 2) → ChoCho
+frontTimes('Chocolate', 3) → ChoChoCho
+frontTimes('Abc', 3) → AbcAbcAbc
+*/
+function frontTimes(str, n) {
+
+  let fronts = str.substring(0, 3);
+  let count = '';
+
+  for (let i = 0; i < n; i++) {
+    count = count + fronts;
+  }
+  return count;
+
+}
+console.log(frontTimes("Toytrain", 2))
